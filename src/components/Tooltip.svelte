8e8a438d092c597977954053bb3a289f71bb3518
aisvarya <aisvarya@MacBookPro.lan>
2025-04-10 12:07:43 -0400

    editing categories

--- src/components/Tooltip.svelte
+++ src/components/Tooltip.svelte
@@ -1,56 +1,58 @@
 <script>
     export let text = '';
     export let position = 'bottom';
-  </script>
-  
-  <div class="tooltip-container">
+</script>
+
+<div class="tooltip-container">
     <slot></slot>
     <div class="tooltip" class:bottom={position === 'bottom'}>
-      {text}
+        {text}
     </div>
-  </div>
-  
-  <style>
+</div>
+
+<style>
     .tooltip-container {
-      position: relative;
-      display: inline-block;
+        position: relative;
+        display: inline-block;
+        font-family: 'Helvetica Neue', sans-serif;
     }
-  
+
     .tooltip {
-      visibility: hidden;
-      position: absolute;
-      background-color: white;
-      color: #374151;
-      text-align: left;
-      padding: 0.75rem 1rem;
-      font-size: 0.875rem;
-      width: max-content;
-      max-width: 300px;
-      border: 1px solid #43485A;
-      box-shadow: 4px 4px 0 0 #d6613b;
-      opacity: 0;
-      transition: opacity 0.2s ease;
-      pointer-events: none;
-      font-weight: 500;
-      z-index: 2000;
+        visibility: hidden;
+        position: absolute;
+        background-color: white;
+        color: #374151;
+        text-align: left;
+        padding: 0.75rem 1rem;
+        font-size: 0.875rem;
+        width: max-content;
+        max-width: 300px;
+        border: 1px solid #43485A;
+        box-shadow: 4px 4px 0 0 #d6613b;
+        opacity: 0;
+        transition: opacity 0.2s ease;
+        pointer-events: none;
+        font-weight: 500;
+        z-index: 2000;
+        font-family: 'Helvetica Neue', sans-serif;
     }
-  
+
     .tooltip.bottom {
-      top: 100%;
-      left: 50%;
-      transform: translateX(-50%);
-      margin-top: 10px;
+        top: 100%;
+        left: 50%;
+        transform: translateX(-50%);
+        margin-top: 10px;
     }
-  
+
     .tooltip-container:hover .tooltip {
-      visibility: visible;
-      opacity: 1;
+        visibility: visible;
+        opacity: 1;
     }
-  
+
     @media (max-width: 768px) {
-      .tooltip {
-        font-size: 0.7rem;
-        max-width: 150px;
-      }
+        .tooltip {
+            font-size: 0.7rem;
+            max-width: 150px;
+        }
     }
-  </style>
+</style>
